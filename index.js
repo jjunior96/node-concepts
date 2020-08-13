3e889ded486e4068a0a837545bb417490f2ec13f
Junior <jjalves96@gmail.com>
2020-08-13 09:06:00 -0300

    feat: put method HTTP

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 const express = require('express');
 
 const server = express();
+server.use(express.json());
 
 const users = ['Diego', 'Junior', 'Lucio'];
 
-server.post('/users', (req, res) => {
-  // Request body = { "name": "Junior" }
-  const id = req.params.id;
+// Listagem de todos os usuarios
+server.get('/users', (req, res) => {
+  return res.json(users);
+});
 
-  return res.json({ message: `Buscando o usuário ${id}` });
-})
+// Listagem de um usuario especifico
+server.get('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  return res.json(users[index]);
+});
+
+// Criacao de um usuario
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
+
+// Edicao de um usuario
+server.put('/users/:index', (req, res) => {
+  // Pego o index
+  const { index } = req.params;
+  // Pego o nome do usuario
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+});
 
 server.listen(3333);
 
