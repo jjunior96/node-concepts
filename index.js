e506fb1002d91d8c4ab7d6c53a3d1cf8b59167c2
Junior <jjalves96@gmail.com>
2020-08-13 08:46:27 -0300

    feat: query params

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require('express');
 const server = express();
 
 server.get('/', (req, res) => {
-  return res.json({ test: "Foi" });
+  // Query params = ?teste=1
+  const name = req.query.name;
+
+  return res.json({ name: name });
 })
 
 server.listen(3333);
+
